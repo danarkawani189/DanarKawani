5ba93e01d934b57c514f238467e9d0a78f22d6ed
Glitch (steep-peach-vinyl) <none>
2023-01-26 20:49:01 +0000

    🎰🏐 Checkpoint ./lib/new.html:28120010/9858 ./index.html:28120010/1289 ./assets/js/routes.js:28120010/1298 ./lib/apps.html:28120010/42 ./lib/game.html:28120010/24 ./lib/jailbreak.html:28120010/24 ./lib/socail.html:28120010/24 ./lib/store.html:28120010/24

--- assets/js/routes.js
+++ assets/js/routes.js
@@ -12,36 +12,20 @@ routes = [
     url: "./lib/apps.html",
   },
     {
-    path: "/gamea/",
+    path: "/game/",
     url: "./lib/game.html",
   },
-      {
-    path: "/ios16/",
-    url: "./pages/ios16.html",
-  },
   {
-    path: "/tor/",
-    url: "./lib/tor.html",
-  },
-  {
-    path: "/version/",
-    url: "./lib/version.html",
+    path: "/socail/",
+    url: "./lib/socail.html",
   },
   {
     path: "/store/",
     url: "./lib/store.html",
   },
-    {
-    path: "/Cydia/",
-    url: "./lib/cydia.html",
-  },
   {
     path: "/pubg/",
     url: "./lib/pubg.html",
-  },
-  {
-    path: "/server/",
-    url: "./lib/server.html",
   },
   {
     path: "/new/",
@@ -60,88 +44,36 @@ routes = [
     url: "./pages/unc0ver.html",
   },
   {
-    path: "/unc0ver1/",
-    url: "./pages/unc0ver1.html",
-  },
-  {
     path: "/Taurin/",
-    url: "./pages/Taurin.html",
-  },
-  {
-    path: "/Taurin1/",
-    url: "./pages/Taurin1.html",
+    url: "./pages/Taurine.html",
   },
   {
     path: "/videostarari/",
     url: "./pages/videostarari.html",
   },
   {
-    path: "/videostar1/",
-    url: "./pages/videostar1.html",
-  },
-  {
     path: "/facebook/",
     url: "./pages/facebook.html",
-  },
-   {
-    path: "/org/",
-    url: "./pages/org.html",
   },
   {
     path: "/Scarlet1/",
     url: "./pages/Scarlet.html",
   },
   {
-    path: "/instagram1/",
-    url: "./pages/instagram1.html",
-  },
-  {
-    path: "/ios15/",
-    url: "./pages/ios15.html",
-  },
-  {
-    path: "/ios14/",
-    url: "./pages/ios14.html",
-  },
-  {
-    path: "/ios13/",
-    url: "./pages/ios13.html",
-  },
-  {
-    path: "/prsyar/",
-    url: "./lib/prsyar.html",
-  },
-  {
-    path: "/ios/",
-    url: "./lib/ios.html",
+    path: "/instagram/",
+    url: "./pages/instagram.html",
   },
   {
     path: "/kinemaster/",
     url: "./pages/kinemaster.html",
-  },
-    {
-    path: "/danabazan/",
-    url: "./pages/danabazan.html",
   },
   {
     path: "/videostar/",
     url: "./pages/videostar.html",
   },
   {
-    path: "/pubg1/",
-    url: "./pages/pubg1.html",
-  },
-  {
-    path: "/pubgvn/",
-    url: "./pages/pubgvn.html",
-  },
-  {
-    path: "/cydiaprsyar/",
-    url: "./pages/cydiaprsyar.html",
-  },
-  {
-    path: "/cydiainfo/",
-    url: "./pages/cydiainfo.html",
+    path: "/pubg/",
+    url: "./pages/pubgg.html",
   },
   {
     path: "/picsart/",
@@ -153,7 +85,7 @@ routes = [
   },
   {
     path: "/spotify/",
-    url: "./pages/spotify1.html",
+    url: "./pages/spotify.html",
   },
   {
     path: "/youtube/",
@@ -163,23 +95,10 @@ routes = [
     path: "/esing/",
     url: "./pages/esing.html",
   },
-  {
-    path: "/fix/",
-    url: "./pages/fix.html",
-  },
-  {
-    path: "/dawakari1/",
-    url: "./lib/dawakari1.html",
-  },
   // Default route (404 page). MUST BE THE LAST
   {
     path: "(.*)",
     url: "./lib/404.html",
-  },
-
-  {
-    path: "/up/",
-    url: "./pages/newupdet.html",
   },
 
 ];
